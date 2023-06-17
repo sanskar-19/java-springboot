b8d9f75c87f267d31fdd3dcc980e3ea68e94393a
Sanskar <sanskaaragrawal2001@gmail.com>
2023-06-17 14:23:14 +0530

    Integrating Swagger UI

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Employees API", version = "2.0", description = "Employees Information"))
 public class DemoApplication {
 
 	public static void main(String[] args) {
