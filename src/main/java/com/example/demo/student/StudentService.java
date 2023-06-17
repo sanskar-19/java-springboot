ba375d790f98b51558f393921afca85674d2503b
Sanskar <sanskaaragrawal2001@gmail.com>
2023-06-17 13:50:55 +0530

    Adding routes

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -19,6 +20,23 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
 
+    public void addNewStudent(Student student){
+        Optional<Student> studentFromDB = studentRepository.findStudentByEmail(student.getEmail());
+
+        if(studentFromDB.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+        System.out.println(student);
+    }
+
+    public void deleteStudent(Long id){
+        boolean exists = studentRepository.existsById(id);
+        if(!exists){
+            throw new IllegalStateException("No student found");
+        }
+        studentRepository.deleteById(id);
     }
 }
